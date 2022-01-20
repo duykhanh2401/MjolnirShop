30dad400f1c06416b539f68c329ce4bd8cf173a8
Duy Khanh <vuduykhanh3@gmail.com>
2022-01-20 15:27:55 +0700

    Update

--- public/js/util/fetchAPI.js
+++ public/js/util/fetchAPI.js
@@ -1,24 +1,24 @@
 export const getDataAPI = async (url) => {
-	const res = await axios.get(`http://localhost:8000/api/v1/${url}`);
+	const res = await axios.get(`/api/v1/${url}`);
 	return res;
 };
 
 export const postDataAPI = async (url, data) => {
-	const res = await axios.post(`http://localhost:8000/api/v1/${url}`, data);
+	const res = await axios.post(`/api/v1/${url}`, data);
 	return res;
 };
 
 export const patchDataAPI = async (url, data) => {
-	const res = await axios.patch(`http://localhost:8000/api/v1/${url}`, data);
+	const res = await axios.patch(`/api/v1/${url}`, data);
 	return res;
 };
 
 export const putDataAPI = async (url, data) => {
-	const res = await axios.put(`http://localhost:8000/api/v1/${url}`, data);
+	const res = await axios.put(`/api/v1/${url}`, data);
 	return res;
 };
 
 export const deleteDataAPI = async (url, data) => {
-	const res = await axios.delete(`http://localhost:8000/api/v1/${url}`, data);
+	const res = await axios.delete(`/api/v1/${url}`, data);
 	return res;
 };
